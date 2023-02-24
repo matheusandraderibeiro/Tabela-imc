f610fc39b886ae063de3b6edde2929bb4c1011ec
matheusandraderibeiro <matheus.a.r.01020@gmail.com>
2023-02-24 15:53:33 -0300

    código js refeito para fins didaticos, nenhuma alteração visual

--- js/resgataInfo.js
+++ js/resgataInfo.js
@@ -1,43 +1,43 @@
-/*var botao = document.querySelector ("#adicionar-paciente");
+const botao = document.querySelector ("#adicionar-paciente");
 
 botao.addEventListener ("click", function (event) {
     event.preventDefault ();
 
-    var form = document.querySelector ("#formulario")
-    var paciente = guardaInformacoesDoPaciente (form);
-    var pacienteTr = criaTrDoFormulario (paciente);
-    var tabela = document.querySelector ("#pacientesRegistrados")
+    const form = document.querySelector ("#formulario");
+    const paciente = guardaInformacoesDoPaciente (form);
+    const pacienteTr = montaTr (paciente);
+    const tabela = document.querySelector ("#pacientesRegistrados");
 
     tabela.appendChild (pacienteTr);
     
-    form.reset ();
-})
+    form.reset();
+})      
 
 function guardaInformacoesDoPaciente (form) {
     var paciente = {
         nome: form.nome.value,
-        peso:form.peso.value,
-        altura:form.altura.value,
+        peso: form.peso.value,
+        altura: form.altura.value, 
         imc: calculaImc (form.peso.value,form.altura.value)
     }
     return paciente;
 }
 
-function criaTrDoFormulario (paciente) {
-    var pacienteTr = document.createElement ("tr");
+function montaTr (paciente) {
+    const pacienteTr = document.createElement ("tr");
     pacienteTr.classList.add ("paciente");
 
-    pacienteTr.appendChild (montaTd (paciente.nome,"info-nome"));
-    pacienteTr.appendChild (montaTd (paciente.peso,"info-peso"));
-    pacienteTr.appendChild (montaTd (paciente.altura,"info-altura"));
-    pacienteTr.appendChild (montaTd (paciente.imc,"info-imc"));
+    pacienteTr.appendChild (criaTd (paciente.nome, "info-nome"));
+    pacienteTr.appendChild (criaTd (paciente.peso, "info-peso"));
+    pacienteTr.appendChild (criaTd (paciente.altura, "info-altura"));
+    pacienteTr.appendChild (criaTd (paciente.imc, "info-imc"));
 
     return pacienteTr;
 }
 
-function montaTd (dado,classe) {
+function criaTd (dado, classe) {
     var td = document.createElement ("td");
-    td.textContent = dado;
+    td.textContent = (dado);
     td.classList.add = (classe);
     return td;
-}*/
+}
--- js/validaInfo.js
+++ js/validaInfo.js
@@ -23,91 +23,13 @@ for (i = 0; i < pacientes.length; i++) {
     }
     
     if (validaPeso && validaAltura) {
-        const imc = (peso / (altura * altura));
-        tdImc.textContent = imc.toFixed(2);
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*var pacientes = document.querySelectorAll (".paciente");
-
-for (var i = 0; i < pacientes.length; i++) {
-    var paciente = pacientes[i];
-    var tdPeso = document.querySelector (".info-peso");
-        var peso = tdPeso.textContent;
-            var pesoValido = true;
-    var tdAltura = document.querySelector (".info-altura");
-        var altura = tdAltura.textContent;
-            var alturaValida = true;
-    var tdImc = document.querySelector (".info-imc");
-
-    if (peso <= 0 || peso >= 200) {
-        pesoValido = false;
-        tdImc.textContent = "Peso Invalido";
-        paciente.classList.add ("paciente-Invalido");
-    }
-
-    if (altura <= 0.70 || altura >=3.00) {
-        alturaValida = false;
-        tdImc.textContent = "altura invalida";
-        paciente.classList.add ("paciente-Invalido");
-    }
-
-    if (pesoValido && alturaValida) {
-        var imc  = calculaImc (peso,altura);
+        var imc = calculaImc (peso, altura);
         tdImc.textContent = imc;
     }
 }
 
 function calculaImc (peso, altura) {
     var imc = 0;
-    var imc = (peso/(altura*altura));
+    imc = (peso / (altura * altura));
     return imc.toFixed(2);
-}*/
+}
